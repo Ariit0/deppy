f3daed75a8ea065fc8ddd7b49ae612361cbb0a27
Ariito <luangamath.ari@gmail.com>
2023-03-18 20:31:37 +1000

    Add publish script

---
+++ publish.sh
@@ -0,0 +1,12 @@
+#!/bin/bash                                                                                                                                                                                                     
+# exit when any command fails
+set -e
+
+# Run relative to the location of this script.
+cd "$(dirname "$(readlink -f "$0")")"
+
+# Build and publish this project to NPM.
+rm -Rdf ./dist/*
+yarn build
+chmod +x ./dist/index.js
+yarn publish --public
